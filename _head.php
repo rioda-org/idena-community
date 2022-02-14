f86dcd5055305dee461fefed338b1ce3d94b0d58
rioda-org <dario.alispahic@gmail.com>
2022-02-14 04:09:44 +0100

    fix icon

--- _head.php
+++ _head.php
@@ -11,7 +11,7 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-  <link rel="shortcut icon" href="/images/icon-32.png"/>
+  <link rel="shortcut icon" href="<?php echo $url; ?>/images/icon-32.png"/>
   <noscript><meta http-equiv="refresh" content="0;url=jscript-disabled.php"></noscript>
     <style>
 	html {
